d990e066a768e1984a6fa39a33e1e9defe2bdde9
williamherring <william.herring.au@gmail.com>
2022-01-02 15:13:43 +1100

    token auth endpoints added

--- server/api/urls.py
+++ server/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('team', TeamView.as_view()),
     path('create-team', CreateTeamView.as_view()),
     path('get-team', GetTeamView.as_view()),
-    path('user', UserView.as_view())
+    path('user', UserView.as_view()),
+    path('create-user', CreateUserView.as_view())
 ]
--- server/server/urls.py
+++ server/server/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
 ]
