0a48c97dd73bb1993383fb7107d5a5654281e904
LAPTOP-J946QDU5\Alec <jeffmills033@gmail.com>
2022-01-04 13:58:51 +1100

    Issue #3 - create login screen UI

---
+++ client/lib/screens/login.dart
@@ -0,0 +1,34 @@
+import 'package:bor/nav/login_form.dart';
+import 'package:bor/objects/team_obj.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leadingWidth: 400,
+          leading: Container(
+            margin: const EdgeInsets.all(5),
+            child: Text(
+              "Bor",
+              style: GoogleFonts.ubuntu(
+                  color: Colors.deepPurpleAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 46
+              ),
+            ),
+          ),
+        ),
+
+        body: Center(
+          child: const LoginForm(),
+        )
+    );
+  }
+}
