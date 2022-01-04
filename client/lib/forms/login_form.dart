883425182a1bd160276b0d731f0108788ca0b81a
williamherring <william.herring.au@gmail.com>
2022-01-04 16:58:07 +1100

    Removed unnecessary widgets

--- client/lib/forms/login_form.dart
+++ client/lib/forms/login_form.dart
@@ -12,16 +12,7 @@ class LoginForm extends StatefulWidget {
 class _LoginFormState extends State<LoginForm> {
   String userInput = "";
   String passInput = "";
-  int stackIndex = 0;
   final _formKey = GlobalKey<FormState>();
-  
-  _LoginFormState({this.userInput = "",this.passInput=""});
-
-  @override
-  void initState() {
-    super.initState();
-    //left this here because idk if there's anything that needs to be added here
-  }
 
 
   @override
@@ -30,135 +21,127 @@ class _LoginFormState extends State<LoginForm> {
       padding: const EdgeInsets.all(3.5),
       width: 400,
       height: 400,
-      child: IndexedStack(
-        index: stackIndex,
-        key: ValueKey<int>(stackIndex),
+      child: Form(
+        key:_formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Log in",
+              style: GoogleFonts.ubuntu(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w500
+              ),
+            ),
 
-        children: [
-          Form(
-            key:_formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Log in",
-                  style: GoogleFonts.ubuntu(
-                    fontSize: 25.0,
+            TextFormField(
+              enableSuggestions: false,
+              autocorrect: false,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter a valid username";
+                }
+
+                if (value.isNotEmpty) {
+                  setState(() {
+                    userInput = value;
+                  });
+                }
+
+                return "Please enter a valid username";
+              },
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                labelText: "Username",
+                labelStyle: GoogleFonts.ubuntu(),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.deepPurpleAccent)
+                ),
+                floatingLabelStyle: GoogleFonts.ubuntu(
+                  color: Colors.deepPurpleAccent
+                ),
+              ),
+
+            ),
+            Text(
+              "",
+                style: GoogleFonts.ubuntu(
+                    fontSize: 11.0,
                     fontWeight: FontWeight.w500
-                  ),
-                ),
-
-                TextFormField(
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter a valid username";
-                    }
-
-                    if (value.isNotEmpty) {
-                      setState(() {
-                        userInput = value;
-                      });
-                    }
-
-                    return "Please enter a valid username";
-                  },
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    labelText: "Username",
-                    labelStyle: GoogleFonts.ubuntu(),
-                    focusedBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.deepPurpleAccent)
-                    ),
-                    floatingLabelStyle: GoogleFonts.ubuntu(
-                      color: Colors.deepPurpleAccent
-                    ),
-                  ),
-
-                ),
-                Text(
-                  "",
-                    style: GoogleFonts.ubuntu(
-                        fontSize: 11.0,
-                        fontWeight: FontWeight.w500
-                    )
-                ),
-
-                TextFormField(
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter a valid password";
-                    }
-
-                    if (value.isNotEmpty) {
-                      setState(() {
-                        passInput = value;
-                      });
-                    }
-
-                    return "Please enter a valid password";
-                  },
-
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    labelStyle: GoogleFonts.ubuntu(),
-                    focusedBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.deepPurpleAccent)
-                    ),
-                    floatingLabelStyle: GoogleFonts.ubuntu(
-                        color: Colors.deepPurpleAccent
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      child: Container(
-                        margin: const EdgeInsets.all(15.0),
-                        child: Text(
-                          "Forgot password?",
-                          style: GoogleFonts.ubuntu(
-                            color: Colors.deepPurpleAccent,
-                          ),
-                        ),
-                      )
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(15.0, 8.0, 15.0, 0.0),
-                      child: ElevatedButton(
-                        onPressed: () => _formKey.currentState!.validate(),
-                        child: Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Text(
-                            "Log in",
-                            style: GoogleFonts.ubuntu(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.deepPurpleAccent,
-                        ),
-                      )
-                    ),
-                  ],
                 )
+            ),
+
+            TextFormField(
+              enableSuggestions: false,
+              autocorrect: false,
+              obscureText: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter a valid password";
+                }
+
+                if (value.isNotEmpty) {
+                  setState(() {
+                    passInput = value;
+                  });
+                }
+
+                return "Please enter a valid password";
+              },
+
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                labelText: "Password",
+                labelStyle: GoogleFonts.ubuntu(),
+                focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepPurpleAccent)
+                ),
+                floatingLabelStyle: GoogleFonts.ubuntu(
+                    color: Colors.deepPurpleAccent
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    child: Text(
+                      "Forgot password?",
+                      style: GoogleFonts.ubuntu(
+                        color: Colors.deepPurpleAccent,
+                      ),
+                    ),
+                  )
+                ),
               ],
             ),
-          ),
-
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(15.0, 8.0, 15.0, 0.0),
+                  child: ElevatedButton(
+                    onPressed: () => _formKey.currentState!.validate(),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text(
+                        "Log in",
+                        style: GoogleFonts.ubuntu(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.deepPurpleAccent,
+                    ),
+                  )
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
