7455b5cdd841d902399648ffb414ec90a26d1cf1
William Herring <73984595+william-herring@users.noreply.github.com>
2022-01-31 14:01:03 +1100

    invite users on members screen

--- client/lib/screens/members.dart
+++ client/lib/screens/members.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:bor/alerts/form_alert.dart';
+import 'package:bor/objects/user_obj.dart';
+import 'package:bor/utils/common_requests.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,18 +16,73 @@ class MemberScreen extends StatefulWidget {
 }
 
 class _MemberScreenState extends State<MemberScreen> {
+  late Future<List<User>> searchResult;
+  List<User> inviteUsers = [];
+  
+  void initState() {
+    super.initState();
+    searchResult = searchUsers("");
+  }
+
+  List<Widget> buildUserTiles(List<User> users) {
+    List<Widget> tiles = [];
+
+    for (var i in inviteUsers) {
+      final username = i.username;
+
+      tiles.add(
+          ListTile(
+            onTap: () {
+              setState(() {
+                inviteUsers.remove(i);
+              });
+            },
+            tileColor: Colors.deepPurpleAccent,
+            iconColor: Colors.white,
+            textColor: Colors.white,
+            leading: const Icon(Icons.account_circle_sharp, size: 30.0),
+            trailing: Text(username, style: GoogleFonts.ubuntu(
+                fontWeight: FontWeight.bold
+            )),
+          )
+      );
+    }
+
+
+    for (var i in users) {
+      final username = i.username;
+
+      List<String> invitedUsernames = [];
+      for (var i in inviteUsers) {
+        invitedUsernames.add(i.username);
+      }
+
+      if (invitedUsernames.contains(username)) {
+        continue;
+      }
+
+      tiles.add(
+          ListTile(
+            onTap: () {
+              setState(() {
+                inviteUsers.add(i);
+              });
+            },
+            tileColor: Colors.transparent,
+            iconColor: getThemeMode() == ThemeMode.dark? Colors.white : Colors.deepPurpleAccent,
+            textColor: getThemeMode() == ThemeMode.dark? Colors.white : Colors.black,
+            leading: const Icon(Icons.account_circle_sharp, size: 30.0),
+            trailing: Text(username, style: GoogleFonts.ubuntu()),
+          )
+      );
+    }
+
+    return tiles;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.outgoing_mail, color: Colors.white),
-        backgroundColor: Colors.deepPurpleAccent,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(16.0))
-        ),
-        onPressed: () {}
-      ),
-
       body: Padding(
         padding: const EdgeInsets.all(32.0),
         child: Column(
@@ -51,6 +111,82 @@ class _MemberScreenState extends State<MemberScreen> {
                   onTap: () {},
                 )
               ],
+            ),
+
+            Container(
+              margin: const EdgeInsets.only(top: 26.0),
+              child: ExpansionTile(
+                textColor: Colors.deepPurpleAccent,
+                iconColor: Colors.deepPurpleAccent,
+                trailing: const Icon(Icons.person_add),
+                title: Text(
+                    "Invite users", style: GoogleFonts.ubuntu()
+                ),
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    //May change to TextFormField
+                    child: TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          searchResult = searchUsers(value);
+                        });
+                      },
+                      decoration: InputDecoration(
+                          icon: const Icon(Icons.search, color: Colors.grey),
+                          focusColor: Colors.deepPurpleAccent,
+                          iconColor: Colors.deepPurpleAccent,
+                          focusedBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepPurpleAccent)
+                          ),
+                          prefixStyle: GoogleFonts.ubuntu(color: Colors.deepPurpleAccent),
+                          hintText: "Search users"
+                      ),
+                    ),
+                  ),
+
+                  FutureBuilder<List<User>> (
+                      future: searchResult,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                          return ListView(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            children: buildUserTiles(snapshot.data!),
+                          );
+                        }
+
+                        return ListView(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          children: buildUserTiles(inviteUsers),
+                        );
+                      }
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          for (var i in inviteUsers) {
+                            inviteUser(currentTeam.code, i.email);
+                          }
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Text(
+                            "Invite",
+                            style: GoogleFonts.ubuntu(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurpleAccent,
+                        ),
+                      )
+                  )
+                ],
+              ),
             ),
           ],
         ),
